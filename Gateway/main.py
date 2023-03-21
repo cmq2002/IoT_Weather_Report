4190ab005a5f699a59c59b2826f9003ca75c167c
Cao Minh Quang <quang.cao2205@gmail.com>
2023-03-21 10:55:20 +0700

    Iot gateway and device driver

---
+++ Gateway/main.py
@@ -0,0 +1,54 @@
+import sys
+import time
+from Adafruit_IO import MQTTClient
+from uart import *
+
+AIO_FEED_IDs = ["button1", "button2", "sensor1", "sensor2", "sensor3"]
+AIO_USERNAME = "quang_cao2002"
+AIO_KEY = "aio_SgOD79eo2vwblPtJWStbEp8KkqXF"
+
+def connected(client):
+    print("Connected ...")
+    for feed in AIO_FEED_IDs:
+        client.subscribe(feed)
+    
+def subscribe(client , userdata , mid , granted_qos):
+    print("Subscribed ...")
+
+def disconnected(client):
+    print("Disconnected ...")
+    sys.exit (1)
+
+def message(client , feed_id , payload):
+    # print("Value Received: " + payload + ", FeedID: " + feed_id)
+    if feed_id == "button1":
+        if payload == "0":
+            writeData(1)
+        else:
+            writeData(2)
+    if feed_id == "button2":
+        if payload == "0":
+            writeData(3)
+        else:
+            writeData(4)
+
+client = MQTTClient(AIO_USERNAME , AIO_KEY)
+client.on_connect = connected
+client.on_disconnect = disconnected
+client.on_message = message
+client.on_subscribe = subscribe
+client.connect()
+client.loop_background()
+
+counter = 20
+while True:
+    #Read data from sensor
+    counter -= 1
+    if (counter <= 0):
+        counter = 20
+        readSerial(client)
+
+    #Send cmd from sever to devices
+    #Implement in function message
+    time.sleep(1)
+    pass
---
+++ Gateway/uart.py
@@ -0,0 +1,51 @@
+import serial.tools.list_ports
+
+def getPort():
+    ports = serial.tools.list_ports.comports()
+    N = len(ports)
+    commPort = "None"
+    for i in range(0, N):
+        port = ports[i]
+        strPort = str(port)
+        if "STMicroelectronics STLink Virtual COM Port" in strPort:
+            splitPort = strPort.split(" ")
+            commPort = (splitPort[0])
+    return commPort
+    # return "COM5"
+
+if getPort() != "None":
+    ser = serial.Serial(port=getPort(), baudrate=9600)
+    print (ser)
+
+def processData(client, data):
+    data = data.replace("!", "")
+    data = data.replace("#", "")
+    splitData = data.split(":")
+    print(splitData)
+    try:
+        if splitData[0] == "TEMP":
+            client.publish("sensor1", splitData[1])
+        elif splitData[0] == "HUMID":
+            client.publish("sensor2", splitData[1])
+        elif splitData[0] == "GAS":
+            client.publish("sensor3", splitData[1])
+    except:
+        pass
+
+mess = ""
+def readSerial(client):
+    bytesToRead = ser.inWaiting()
+    if (bytesToRead > 0):
+        global mess
+        mess = mess + ser.read(bytesToRead).decode("UTF-8")
+        while ("#" in mess) and ("!" in mess):
+            start = mess.find("!")
+            end = mess.find("#")
+            processData(client, mess[start:end + 1])
+            if (end == len(mess)):
+                mess = ""
+            else:
+                mess = mess[end+1:]
+
+def writeData(data):
+    ser.write(str(data).encode())
